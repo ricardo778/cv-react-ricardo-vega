91fd9fb1fa1fd9bd896fb81208ca16f1bb3f0998
av164702gmail.com <av164702gmail.com>
2025-05-21 15:45:21 -0500

    feat: componente Educacion con formación complementaria

---
+++ src/components/Educacion.jsx
@@ -0,0 +1,12 @@
+export default function Educacion() {
+  return (
+    <section>
+      <h3>Educación</h3>
+      <ul>
+        <li>
+          <strong>Tecnologia en Analisis y desarrollo de software</strong> - SENA (CTMA) (2025-2027)
+        </li>
+      </ul>
+    </section>
+  );
+}
