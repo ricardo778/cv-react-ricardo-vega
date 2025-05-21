7ff41e0ac786d496cd4efbe96a71fef83bf0142f
av164702gmail.com <av164702gmail.com>
2025-05-21 15:46:13 -0500

    feat: componente Experiencia con 4 cargos definidos

---
+++ src/components/Experiencia.jsx
@@ -0,0 +1,15 @@
+export default function Experiencia() {
+  return (
+    <section>
+      <h3>Experiencia Profesional</h3>
+      <ul>
+        <li>
+          <strong>Desarrollador Full Stack</strong>
+        </li>
+        <li>
+          <strong>Aprendiz</strong> - SENA (2025-2027)
+        </li>
+      </ul>
+    </section>
+  );
+}
