26cd4c40951162194072893e92a0c9b53b1c6bde
av164702gmail.com <av164702gmail.com>
2025-05-26 15:15:02 -0500

    cambios del perfil

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,6 +1,7 @@
+import './estilos.css';
 export default function Perfil() {
   return (
-    <section>
+    <section className="perfil">
       <h3>Perfil</h3>
       <p>
         Tecnologo en Analisis y Desarrollo de software con experiencia en desarrollo web y móvil, apasionado por la tecnología.
